b3091d7cc84386fc66bd72ac5aa91db579f9348f
werto87 <huhulasmichinruhe@web.de>
2021-12-19 22:13:21 +0100

    init

--- all/conanfile.py
+++ all/conanfile.py
@@ -3,11 +3,11 @@ from conans.tools import check_min_cppstd
 import os
 
 
-class ConfuJson(ConanFile):
-    name = "confu_json"
-    homepage = "https://github.com/werto87/confu_json"
-    description = "uses boost::fusion to help with serialization; json <-> user defined type"
-    topics = ("json parse", "serialization", "user defined type")
+class MatchmakingProxy(ConanFile):
+    name = "matchmaking_proxy"
+    homepage = "https://github.com/werto87/matchmaking_proxy"
+    description = "used to login user and make a match. Then sends data to the user defined game server to start the game."
+    topics = ("server", "login", "proxy", "matchmaking")
     license = "BSL-1.0"
     url = "https://github.com/conan-io/conan-center-index"
     settings = "compiler"
@@ -23,17 +23,30 @@ class ConfuJson(ConanFile):
         self.options["boost"].header_only = True
 
     def requirements(self):
-        self.requires("boost/1.76.0")
+        self.requires("range-v3/0.12.0@werto87/stable")
+        self.requires("certify/0.0.1@werto87/stable")
+        self.requires("boost/1.77.0")
+        self.requires("confu_soci/0.2.0@werto87/stable")
+        self.requires("libsodium/1.0.18")
+        self.requires("confu_json/0.0.8@werto87/stable")
+        self.requires("magic_enum/0.7.2")
+        self.requires("catch2/2.13.7")
+        self.requires("pipes/1.0.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        if self.version != "latest":
+            tools.get(**self.conan_data["sources"][self.version])
+            extracted_dir = self.name + "-" + self.version
+            os.rename(extracted_dir, self._source_subfolder)
+        else:
+            tools.get(url="https://github.com/werto87/matchmaking_proxy/archive/refs/heads/main.zip")
+            extracted_dir = self.name +"-main"
+            os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
         # This should lead to an Include path like #include "include_folder/IncludeFile.hxx"
-        self.copy("*.h*", dst="include/confu_json",
-                  src="source_subfolder/confu_json")
+        self.copy("*.h*", dst="include/"+self.name,
+                  src="source_subfolder/"+self.name)
 
     def package_id(self):
         self.info.header_only()
